d88ad0653ea96d1e94394a2b250ab8648543a5fe
sindhufinland <67472777+sindhuja2002@users.noreply.github.com>
2025-10-26 19:04:04 +0200

    Added GoogleMaps API Key function

--- utils/places_utils.py
+++ utils/places_utils.py
@@ -4,3 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 GOOGLE_KEY = os.getenv("GOOGLE_API_KEY")
+
+
+def _ck(key):
+    if not key: raise RuntimeError("Missing GOOGLE_API_KEY. Add it to .env")
+
+    
